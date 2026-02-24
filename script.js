e4008ea57a3f0dab0daed767cdf5c6f42447af05
Santu666-AI <chari.netvision@gmail.com>
2026-02-24 13:24:07 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -94,11 +94,11 @@ function today(){
 
   const now = new Date();
 
-  const y = now.getFullYear();
-  const m = String(now.getMonth()+1).padStart(2,"0");
-  const d = String(now.getDate()).padStart(2,"0");
+  const year = now.getFullYear();
+  const month = String(now.getMonth()+1).padStart(2,"0");
+  const day = String(now.getDate()).padStart(2,"0");
 
-  return `${y}-${m}-${d}`;
+  return `${year}-${month}-${day}`;
 }
 
 /* ================= TAB SWITCH ================= */
